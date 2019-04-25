f325bc1d8b136581dcf961b65ff8cacc91c140a9
Rakhi32 <35827760+Rakhi32@users.noreply.github.com>
2019-04-25 10:58:21 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="master.css">
+<ul>
+	<li><a href="Home.html">Home</a></li>
+	<li><a href="Academic_information.html">Academic Info</a></li>
+	<li><a href="personal_Information.html">Personal Info</a>
+    <li><a href="Photo_album.html">Photo album</a></li>
+	<li><a href="Interests.html">Interests</a></li>
+	<li><a href="Useful_links.html">Useful Links</a></li>
+	<li><a href="ContactMe.html">ContactMe</a></li>
+
+
+
+
+</ul>
+	</head>
+<body>
+	<?php
+	<div class="prof-box">
+
+	 <marquee> <h1>WELCOME TO MY PERSONAL WEBSITE</h1> </marquee>
+	<center><img src="images/Rakhi Paul.jpg" class="prof-pic"></center>
+	<center><h2>Rakhi Rani Paul</h2></center>
+	<center><h3>Computer Science And Engineering</h3></center>
+	<center><h3>Rajshahi University</h3></center>
+
+	</div>
+?>
+
+</body>
